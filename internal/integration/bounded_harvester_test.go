ef78808c5d4d2bc54584f0826dd4394c936b709a
Roberto Santalla <rsantalla@newrelic.com>
2020-12-15 11:26:49 +0100

    bounded_harvester/test: add unit tests

---
+++ internal/integration/bounded_harvester_test.go
@@ -0,0 +1,176 @@
+package integration
+
+import (
+	"context"
+	"github.com/newrelic/newrelic-telemetry-sdk-go/telemetry"
+	"testing"
+	"time"
+)
+
+type mockHarvester struct {
+	metrics  int
+	harvests int
+}
+
+func (h *mockHarvester) RecordMetric(m telemetry.Metric) {
+	h.metrics++
+}
+
+func (h *mockHarvester) HarvestNow(ctx context.Context) {
+	h.harvests++
+}
+
+// Checks that bindHarvester returns a harvester, correctly overriding settings
+// Also check the async routine is spawned
+func TestBindHarvester(t *testing.T) {
+	cfg := BoundedHarvesterCfg{
+		MetricCap:                0,
+		HarvestPeriod:            1,
+		MinReportInterval:        10,
+		DisablePeriodicReporting: true,
+	}
+
+	mock := &mockHarvester{}
+	h := bindHarvester(mock, cfg)
+
+	bh, ok := h.(*boundedHarvester)
+	if !ok {
+		t.Fatalf("returned harvester is not a boundedHarvester")
+	}
+	defer bh.Stop()
+
+	if bh.MinReportInterval != BoundedHarvesterDefaultMinReportInterval {
+		t.Fatalf("MinReportInterval was not overridden")
+	}
+
+	if bh.HarvestPeriod != BoundedHarvesterDefaultHarvestPeriod {
+		t.Fatalf("HarvestPeriod was not overridden")
+	}
+
+	if bh.MetricCap != BoundedHarvesterDefaultMetricsCap {
+		t.Fatalf("MetricCap was not overridden")
+	}
+
+	time.Sleep(time.Second)
+	if mock.harvests != 0 {
+		t.Fatalf("Periodic routine was called despite being disabled")
+	}
+}
+
+func TestHarvestRoutine(t *testing.T) {
+	cfg := BoundedHarvesterCfg{
+		HarvestPeriod:     300 * time.Millisecond,
+		MinReportInterval: BoundedHarvesterDefaultMinReportInterval,
+	}
+
+	mock := &mockHarvester{}
+	h := bindHarvester(mock, cfg)
+
+	bh, ok := h.(*boundedHarvester)
+	if !ok {
+		t.Fatalf("returned harvester is not a boundedHarvester")
+	}
+	defer bh.Stop()
+
+	time.Sleep(time.Second)
+	if mock.harvests < 1 {
+		t.Fatalf("harvest routine was not called within 1s")
+	}
+}
+
+func TestRoutineStopChannel(t *testing.T) {
+	cfg := BoundedHarvesterCfg{
+		HarvestPeriod:     300 * time.Millisecond,
+		MinReportInterval: BoundedHarvesterDefaultMinReportInterval,
+	}
+
+	mock := &mockHarvester{}
+	h := bindHarvester(mock, cfg)
+
+	bh, ok := h.(*boundedHarvester)
+	if !ok {
+		t.Fatalf("returned harvester is not a boundedHarvester")
+	}
+	defer bh.Stop()
+
+	time.Sleep(time.Second)
+	bh.Stop()
+	time.Sleep(time.Second)
+	harvests := mock.harvests
+	time.Sleep(time.Second)
+	if mock.harvests != harvests {
+		t.Fatalf("Stop() did not stop the harvest routine")
+	}
+}
+
+func TestRoutineStopFlag(t *testing.T) {
+	cfg := BoundedHarvesterCfg{
+		HarvestPeriod:     300 * time.Millisecond,
+		MinReportInterval: BoundedHarvesterDefaultMinReportInterval,
+	}
+
+	mock := &mockHarvester{}
+	h := bindHarvester(mock, cfg)
+
+	bh, ok := h.(*boundedHarvester)
+	if !ok {
+		t.Fatalf("returned harvester is not a boundedHarvester")
+	}
+	defer bh.Stop()
+
+	time.Sleep(time.Second)
+	bh.DisablePeriodicReporting = true
+	time.Sleep(time.Second)
+	harvests := mock.harvests
+	time.Sleep(time.Second)
+	if mock.harvests != harvests {
+		t.Fatalf("DisablePeriodicReporting = true did not stop the harvest routine")
+	}
+}
+
+func TestHarvestNow(t *testing.T) {
+	cfg := BoundedHarvesterCfg{
+		DisablePeriodicReporting: true,
+	}
+
+	mock := &mockHarvester{}
+	h := bindHarvester(mock, cfg)
+
+	bh, ok := h.(*boundedHarvester)
+	if !ok {
+		t.Fatalf("returned harvester is not a boundedHarvester")
+	}
+	defer bh.Stop()
+
+	h.HarvestNow(context.Background())
+	time.Sleep(100 * time.Millisecond) // Inner HarvestNow is asynchronous
+	if mock.harvests < 1 {
+		t.Fatalf("HarvestNow did not trigger a harvest")
+	}
+}
+
+func TestMetricCap(t *testing.T) {
+	cfg := BoundedHarvesterCfg{
+		HarvestPeriod: time.Hour,
+		MetricCap:     3,
+	}
+
+	mock := &mockHarvester{}
+	h := bindHarvester(mock, cfg)
+
+	bh, ok := h.(*boundedHarvester)
+	if !ok {
+		t.Fatalf("returned harvester is not a boundedHarvester")
+	}
+	defer bh.Stop()
+
+	h.RecordMetric(telemetry.Count{})
+	h.RecordMetric(telemetry.Count{})
+	h.RecordMetric(telemetry.Count{})
+	h.RecordMetric(telemetry.Count{})
+	time.Sleep(time.Second) // Wait for MinReportInterval
+
+	if mock.harvests < 1 {
+		t.Fatalf("Stacking metrics did not trigger a harvest")
+	}
+}
