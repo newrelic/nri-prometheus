5d596e6931843e533a2e0537dc927118a6f64788
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2021-06-29 10:57:18 +0200

    tests: add infra sdk emmiter benchamark

--- internal/integration/integration_test.go
+++ internal/integration/integration_test.go
@@ -61,3 +61,58 @@ func do(b *testing.B, retrievers []endpoints.TargetRetriever) {
 		[]Emitter{&nilEmit{}},
 	)
 }
+
+func BenchmarkIntegrationInfraSDKEmitter(b *testing.B) {
+	cachedFile, err := ioutil.ReadFile("test/cadvisor.txt")
+	assert.NoError(b, err)
+	contentLength := strconv.Itoa(len(cachedFile))
+	b.Log("payload size", contentLength)
+	server := httptest.NewServer(http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Set("Content-Length", contentLength)
+		_, err := resp.Write(cachedFile)
+		assert.NoError(b, err)
+	}))
+	defer server.Close()
+
+	fr, err := endpoints.FixedRetriever(endpoints.TargetConfig{URLs: []string{server.URL}})
+	assert.NoError(b, err)
+	var retrievers []endpoints.TargetRetriever
+	for i := 0; i < 20; i++ {
+		retrievers = append(retrievers, fr)
+	}
+
+	sd := []SynthesisDefinition{
+		{
+			EntityRule: EntityRule{
+				EntityType: "CONTAINER",
+				Identifier: "id",
+				Name:       "container_name",
+				Conditions: []Condition{
+					{
+						Attribute: "metricName",
+						Prefix:    "container_",
+					},
+				},
+				Tags: Tags{
+					"namespace":  nil,
+					"targetName": nil,
+				},
+			},
+		},
+	}
+
+	s := NewSynthesizer(sd)
+	emitter := NewInfraSdkEmitter(s)
+	emitters := []Emitter{emitter}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ExecuteOnce(
+			retrievers,
+			NewFetcher(30*time.Second, 5000000000, 4, "", "", false, queueLength),
+			RuleProcessor([]ProcessingRule{}, queueLength),
+			emitters)
+	}
+}
